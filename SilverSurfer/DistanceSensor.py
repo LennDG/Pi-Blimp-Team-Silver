2706c043eecd554c9fa88a91ce4bfeeec95a5d9d
Cookiedude <robcoekaerts@gmail.com>
2013-10-21 15:49:11 +0200

    Voorlopige implementaties, trekt nog nergens op

---
+++ SilverSurfer/DistanceSensor.py
@@ -0,0 +1,10 @@
+
+class DistanceSensor():
+    
+    def __init__(self):
+        pass  #will have something to do with the pins.
+    
+    def heigth(self):
+        return -1 #dummy implementation
+    
+    
---
+++ SilverSurfer/ZeppelinControl.py
@@ -0,0 +1,15 @@
+#This is the file for the ZeppelinControlfile
+import MotorControl
+
+class ZeppelinControl():
+    
+    def __init__(self):
+        
+        self.motor_control = MotorControl()
+        self.current_heigth = 0
+        
+    
+    def stabilise(self):
+        pass
+    
+    
