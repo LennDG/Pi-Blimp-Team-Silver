b5b9c2334dd81ee4afb6135d718ee81f16a80ebb
Cookiedude <robcoekaerts@gmail.com>
2013-11-04 18:49:58 +0100

    documentation

--- SilverSurfer/Commands.py
+++ SilverSurfer/Commands.py
@@ -1,33 +1,63 @@
+'''
+This module contains all the Command classes.
+'''
 
-
+'''
+This class is the Command superclass. All the command classes are child classes from this class. This class defines an execute method
+that must be implemented by all child classes.
+'''
 class Command():
     
     def __init__(self):
         pass
     
+    '''
+    This method must be implemented by all child classes. In its current state, it does not do anything.
+    '''
     def execute(self, zeppelin):
         pass
 
-
+'''
+This class is inherits from Command and is the superclass of all classes that need a certain time to be carried out. During this time,
+other commands should be able to be executed.
+'''
 class TermCommand(Command):
     
+    '''
+    The constructor of this superclass demands a parameter. This parameter will be converted into the time necessary to carry out the
+    command.
+    '''
     def __init__(self, parameter):
         super(TermCommand, self).__init__()
         self.parameter = parameter
     
+    '''
+    This method calculates the time necessary to perform the command based on the parameter given to the constuctor. This method
+    must be implemented by all subclasses.
+    '''
     def calculate_time(self):
         return -1
         print "this method hasn't been implemented yet."
     
-
+'''
+This class describes the move command. A parameter can be supplied which will represent the distance the object that executes 
+this command should move.
+'''
 class Move(TermCommand):
     
-    def __init__(self, parameter):
-        super(TermCommand, self).__init__(parameter)
-        
+    def __init__(self, distance):
+        super(TermCommand, self).__init__(distance)
+    
+    '''
+    This method will initiate the movement of the object that executes this command.
+    '''    
     def execute(self, zeppelin):
         zeppelin.control.move(self.parameter)
-        
+    
+    '''
+    this method calculates the time necessary to move the given distance based on the linear function a*self.parameter + b
+    @return: a*self.parameter + b
+    '''  
     def calculate_time(self):
         
         a = 1  # to be determined through heavy testing
