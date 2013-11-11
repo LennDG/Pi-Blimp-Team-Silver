fa39db1b8d53de69dad0f79456851d1982ebd5ce
jens <jens@10.43.10.226>
2013-11-11 19:45:24 +0100

    Grafiek & kleur & pijltjes

--- SilverSurfer/GUI.py
+++ SilverSurfer/GUI.py
@@ -6,6 +6,71 @@ from ScrolledText import ScrolledText
 import Commands
 import Queue
 import matplotlib.pyplot as pp
+from matplotlib import pyplot
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from numpy import random, sin, exp
+from PIL import Image,ImageTk
+
+class Plotter(Frame) : #te hard coded? herbruikbaar?
+    def __init__(self,parent,max_x,max_y) : #extra method als handler (get height blablabla)
+        Frame.__init__(self,parent)
+        self.update_idletasks()
+        self.x = 0
+        self.i = 0
+        self.left_lim = 0
+        self.update = 10  # to speed things up, never plot more than n_points on screen
+        self.max_line=150
+        self.max_x = max_x  
+        self.interval=max_x  
+        self.min_x = 0      
+        self.xy_data = []
+        self.figure = pyplot.figure()
+        # figsize (w,h tuple in inches) dpi (dots per inch)
+        self.figure.set_size_inches((5,5), dpi=100, forward=True)
+        self.subplot = self.figure.add_subplot(111)
+        self.line, = self.subplot.plot([],[])
+        pyplot.xlim(self.min_x,self.max_x)
+        pyplot.ylim(0,max_y)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self)
+        self.stop = True
+        self.canvas.get_tk_widget().grid()
+
+    def plotter(self):
+        self.x += 1
+        self.y = abs(random.randn())
+        self.xy_data += [[self.x,self.y]]
+            
+        self.min_x = self.x-self.interval
+        self.max_x = self.x
+        self.left_lim=self.i -self.interval
+            
+            
+        if self.i<self.interval:
+            self.left_lim =0
+            self.min_x = 0
+            self.max_x = self.interval
+  
+            
+        if self.i>self.max_line:
+            self.xy_data= self.xy_data[self.i-self.interval:self.i]
+            self.left_lim=0
+            self.i = self.interval
+        
+        
+            
+        self.subplot.lines.remove(self.line) 
+        pyplot.xlim(self.min_x,self.max_x)
+        self.line, = self.subplot.plot(
+                        [row[0] for row in self.xy_data[self.left_lim:self.i]],
+                            [row[1] for row in self.xy_data[self.left_lim:self.i]],
+                            color="blue")
+        self.i += 1
+        self.canvas.draw()
+        self.canvas.get_tk_widget().update_idletasks()
+       
+        if self.stop == False:
+            self.after(10, self.plotter)
 
 
 class GUI(Frame):
@@ -14,7 +79,7 @@ class GUI(Frame):
     def __init__(self, queue,zeppelin): 
         self.parent = Tk() 
         self.parent.geometry("1300x650+300+300") 
-        Frame.__init__(self, self.parent, background="white") 
+        Frame.__init__(self, self.parent, background="gray55") 
         self.zeppelin=zeppelin
         self.initGUI() 
         self.queue=queue
@@ -27,6 +92,7 @@ class GUI(Frame):
         
         #flag_btn init op false
         self.flag_btn = False
+        self.stop_show_height = True
         
         #binden van buttons
         self.parent.bind('<Up>',self.move_forward)
@@ -47,37 +113,35 @@ class GUI(Frame):
         
         
         #input
-        self.Frame_input = Frame(self,background="white")
+        self.Frame_input = Frame(self,background="gray55")
         self.Frame_input.grid(row = 0, column = 0, sticky='W') 
         
-        
-        lbl_image = Label(self.Frame_input, text="Afbeelding") #Text="Afbeelding" moet vervangen worden door image=... 
-        lbl_image.config(width = 70, height = 30) 
+        self.img1 = Image.open('sample.png')
+        imgr = self.img1.resize((400, 400),Image.ANTIALIAS)
+        self.img = ImageTk.PhotoImage(imgr)
+        lbl_image = Label(self.Frame_input, image=self.img) #TODO: image juist adress. 
+        #lbl_image = Label(self.Frame_input, text="Afbeelding")
+        #lbl_image.config(width = 70, height = 30) 
         lbl_image.grid(row = 0, column = 0, padx = 5, pady = 5, columnspan=3) 
         
         # 3 Menu knoppen 
         menu_btn_width = 7 
         menu_btn_height = 1
         
-        self.Frame_cmenu = Frame(self.Frame_input,background="white")
+        self.Frame_cmenu = Frame(self.Frame_input,background="gray55")
         self.Frame_cmenu.config(width= 1)
         self.Frame_cmenu.grid(row = 1, column = 0,  sticky='W') 
-
-      
-        btn_read_qr = Button(self.Frame_cmenu, text="READ QR") 
-        btn_read_qr.config( height = menu_btn_height, width = menu_btn_width ) 
-        btn_read_qr.grid(row = 0, column = 0, padx = 5, pady = 3, columnspan = 1, sticky='W') 
         
-        btn_record = Button(self.Frame_cmenu, text="RECORD") 
+        btn_record = Button(self.Frame_cmenu, text="RECORD",background ="gray11",foreground = "white") 
         btn_record.config( height = menu_btn_height, width = menu_btn_width ) 
         btn_record.grid(row = 1, column = 0, padx = 5, pady = 3, columnspan = 1,sticky='W') 
         
-        btn_pic = Button(self.Frame_cmenu, text="TAKE PIC") 
+        btn_pic = Button(self.Frame_cmenu, text="TAKE PIC",background ="gray11",foreground = "white") 
         btn_pic.config( height = menu_btn_height, width = menu_btn_width ) 
         btn_pic.grid(row = 2, column = 0, padx = 5, pady = 3, columnspan = 1,sticky='W') #Manual input 
        
        
-        btn_command = Button(self.Frame_cmenu, text="COMMAND" , command= self.invoke_command )
+        btn_command = Button(self.Frame_cmenu, text="COMMAND" , command= self.invoke_command,background ="gray11",foreground = "white" )
         btn_command.config( height = menu_btn_height, width = menu_btn_width + 3 ) 
         btn_command.grid(row = 3, column = 0, padx = 5, pady = 3, sticky='W') 
           
@@ -86,48 +150,59 @@ class GUI(Frame):
         entry_input = Entry(self.Frame_input) 
         entry_input.config( width = 1 )
         entry_input.grid(row = 4, column = 0,columnspan=2, padx = 3, pady = 3,sticky="WE") 
-        btn_input_enter = Button(self.Frame_input, text="ENTER") 
+        btn_input_enter = Button(self.Frame_input, text="ENTER",background ="gray11",foreground = "white") 
         btn_input_enter.grid(row = 4, column = 2, padx = 2, pady = 3,sticky="WE") #pijltjes 
        
 #Grote Stop knop
        
        
-        btn_stop = Button(self.Frame_input, text="STOP" , command= self.stop )
+        btn_stop = Button(self.Frame_input, text="STOP" , command= self.stop, background = "red",foreground = "white")
         btn_stop.config( height = 5, width = 10) 
         btn_stop.grid(row = 1, column = 1, padx = 5, pady = 3, sticky='W') 
        
 #pijltjes, A en D
        
-        self.Frame_btn_control = Frame(self.Frame_input,background="white")
-        self.Frame_btn_control.grid(row = 1, column = 2, rowspan = 3, columnspan = 3) 
+        self.Frame_btn_control = Frame(self.Frame_input,background="gray55")
+        self.Frame_btn_control.grid(row = 1, column = 2) 
         
-        rc_btn_height = 2 
-        rc_btn_width = 4 
+        rc_btn_height = 32 
+        rc_btn_width = 34 
         
-        self.btn_left = Button(self.Frame_btn_control, text="LEFT") #pijltje omhoog afbeelding 
+        self.img_left = Image.open('draai.png')
+        imgr_left = self.img_left.resize((50, 50),Image.ANTIALIAS)
+        self.img_left1 = ImageTk.PhotoImage(imgr_left)
+        self.btn_left = Button(self.Frame_btn_control, image=self.img_left1,background ="gray11",foreground = "white") #pijltje omhoog afbeelding #TODO:
         self.btn_left.config( height = rc_btn_height, width = rc_btn_width ) 
         self.btn_left.grid(row = 1, column = 0,padx = 5, pady = 3)
 
-        self.btn_down = Button(self.Frame_btn_control, text="BACK") #pijltje beneden afbeelding 
+        self.img_down = Image.open('pijl2.png')
+        imgr_down = self.img_down.resize((50, 50),Image.ANTIALIAS)
+        self.img_down1 = ImageTk.PhotoImage(imgr_down)
+        self.btn_down = Button(self.Frame_btn_control, image=self.img_down1,background ="gray11",foreground = "white") #pijltje beneden afbeelding 
         self.btn_down.config( height = rc_btn_height, width = rc_btn_width ) 
         self.btn_down.grid(row = 1, column = 1,padx = 5, pady = 3) 
         
-        self.btn_up = Button(self.Frame_btn_control, text="FORW") #pijltje omhoog afbeelding 
+        self.img_up = Image.open('pijl1.png')
+        imgr_up = self.img_up.resize((50, 50),Image.ANTIALIAS)
+        self.img_up1 = ImageTk.PhotoImage(imgr_up)
+        self.btn_up = Button(self.Frame_btn_control, image=self.img_up1,background ="gray11",foreground = "white") #pijltje omhoog afbeelding 
         self.btn_up.config( height = rc_btn_height, width = rc_btn_width ) 
         self.btn_up.grid(row = 0, column = 1 ,padx = 5, pady = 3)
         
         
-        
-        self.btn_right = Button(self.Frame_btn_control, text="RIGHT") #pijltje rechts afbeelding 
+        self.img_right = Image.open('draai2.png')
+        imgr_right = self.img_right.resize((50, 50),Image.ANTIALIAS)
+        self.img_right1 = ImageTk.PhotoImage(imgr_right)
+        self.btn_right = Button(self.Frame_btn_control, image=self.img_right1,background ="gray11",foreground = "white") #pijltje rechts afbeelding 
         self.btn_right.config( height = rc_btn_height, width = rc_btn_width ) 
         self.btn_right.grid(row = 1, column = 2 ,padx = 5, pady = 3) 
         
-        self.btn_ascend = Button(self.Frame_btn_control, text="A") #stijgen
-        self.btn_ascend.config( height = rc_btn_height, width = rc_btn_width ) 
+        self.btn_ascend = Button(self.Frame_btn_control, text="A",background ="gray11",foreground = "white") #stijgen
+        self.btn_ascend.config( height = 2, width = 4 ) 
         self.btn_ascend.grid(row = 2, column = 0,padx = 5, pady = 3)
         
-        self.btn_descend = Button(self.Frame_btn_control, text="D") #dalen
-        self.btn_descend.config( height = rc_btn_height, width = rc_btn_width ) 
+        self.btn_descend = Button(self.Frame_btn_control, text="D",background ="gray11",foreground = "white") #dalen
+        self.btn_descend.config( height = 2, width = 4 ) 
         self.btn_descend.grid(row = 2, column = 2 ,padx = 5, pady = 3)
         
         #binden GUI-stuurknoppen
@@ -151,22 +226,62 @@ class GUI(Frame):
         self.btn_descend.bind("<ButtonRelease-1>", self.v_release)
         
         #output
-        Frame_output = Frame(self,background="white")
-        Frame_output.grid(row = 0, column = 1,  sticky='WE') 
+        self.Frame_output = Frame(self,background="gray55")
+        self.Frame_output.grid(row = 0, column = 1,  sticky='WE') 
         
-        output = ScrolledText(Frame_output, undo=True, state='disabled')
-        output['font'] = ('consolas', '12')
-        output.config(width = 70, height = 30) 
-        output.grid(row = 0, column = 0, padx = 5, pady = 5, columnspan = 2,sticky='WE') 
+        #TODO: Frame pictures motors
         
-        btn_h = Button(Frame_output, text="H") 
-        btn_h.config( height = rc_btn_height, width = rc_btn_width ) 
-        btn_h.grid(row = 1, column = 0,padx = 5, pady = 3,sticky='WE')
+        self.output = ScrolledText(self.Frame_output, undo=True, state='disabled')
+        self.output['font'] = ('consolas', '12')
+        self.output.config(width = 70, height = 30) 
+        self.output.grid(row = 0, column = 0, padx = 5, pady = 5, columnspan = 3,sticky='WE') 
         
-        txt_ct = Text(Frame_output, undo=True, state='disabled', Text = self.zeppelin.height)
-        txt_ct['font'] = ('consolas', '12')
-        txt_ct.config(width = 20, height = 1) 
-        txt_ct.grid(row = 1, column = 1, padx = 5, pady = 5,sticky='WE') 
+        #Grafiek
+        self.Frame_visual_view=Frame(self.Frame_output,background="gray55")
+        
+        self.Frame_motors = Frame(self.Frame_visual_view,background="gray55")
+        
+        self.motor1 = StringVar()
+        self.motor1.set('...')
+        
+        self.motor2 = StringVar()
+        self.motor2.set('...')
+        
+        self.motor3 = StringVar()
+        self.motor3.set('...')
+        
+        self.lbl_motor1 = Label(self.Frame_motors, textvariable=self.motor1)
+        self.lbl_motor1.grid(row = 0, column = 0, padx = 5, pady = 5,sticky='WE') 
+        
+        self.lbl_motor2 = Label(self.Frame_motors, textvariable=self.motor2)
+        self.lbl_motor2.grid(row = 0, column = 1, padx = 5, pady = 5,sticky='WE') 
+        
+        self.lbl_motor3 = Label(self.Frame_motors, textvariable=self.motor3)
+        self.lbl_motor3.grid(row = 0, column = 2, padx = 5, pady = 5,sticky='WE') 
+        
+        self.Frame_graphview = Frame(self.Frame_visual_view,background="gray55")
+        self.Frame_graphview.config(width=800,height = 500)
+        self.height_graph = Plotter(self.Frame_graphview,50,3)
+        
+        self.view_state = 'output'  
+        btn_h = Button(self.Frame_output, text="CHANGE VIEW", command = self.invoke_change_view,background ="gray11",foreground = "white") 
+        btn_h.config( height = 2, width = 4 ) 
+        btn_h.grid(row = 1, column = 0,padx = 5, pady = 3,sticky='WSE')
+        
+        btn_h = Button(self.Frame_output, text="SHOW H", command = self.invoke_show_height,background ="gray11",foreground = "white") 
+        btn_h.config( height = 2, width = 4 ) 
+        btn_h.grid(row = 1, column = 1,padx = 5, pady = 3,sticky='WSE')
+        
+#         self.txt_ct = Text(self.Frame_output, undo=True, state='disabled')
+#         self.txt_ct['font'] = ('consolas', '12')
+#         self.txt_ct.config(width = 20, height = 1) 
+#         self.txt_ct.grid(row = 1, column = 2, padx = 5, pady = 5,sticky='WSE') 
+
+        self.height = StringVar()
+        self.height.set('...')
+        self.lbl_height = Label(self.Frame_output, textvariable=self.height)
+        self.lbl_height.config(width=6)
+        self.lbl_height.grid(row = 1, column = 2, padx = 5, pady = 5,sticky='WE')  
         
         
         
@@ -183,34 +298,76 @@ class GUI(Frame):
         self.Frame_input.grid_columnconfigure(1,weigh=1)
         
        
+    def invoke_change_view(self):
+        if self.view_state is 'output':
+            self.invoke_change_view_to_graph()
+        else: 
+            self.invoke_change_view_to_output_text()
+            
+    def invoke_show_height(self):
+        if self.stop_show_height == True:
+            self.stop_show_height=False 
+            self.show_height()
+        else: 
+            self.stop_show_height=True
+            self.height.set('...')
         
     
+    def show_height(self):
+        if self.stop_show_height == False:
+            height = random.randint(0,11)
+            self.height.set(height) 
+            self.after(50,self.show_height)
+        
+    def invoke_change_view_to_graph(self):
+        self.Frame_visual_view.grid(row = 0, column = 0, columnspan = 3,sticky='WE') 
+        self.Frame_motors.grid(row=1)
+        self.Frame_graphview.grid(row=0,column=0)
+        self.height_graph.grid(row=0,column=0)        
+        self.output.grid_remove()  
+        self.show_graphs() 
+        self.view_state = 'graph'  
+        
+    def invoke_change_view_to_output_text(self):
+        self.stop_graphs()
+        self.Frame_visual_view.grid_remove()
+        self.output.grid(row = 0, column = 0, padx = 5, pady = 5, columnspan = 3,sticky='WE')         
+        self.view_state = 'output'   
+      
+    
+    def show_graphs(self):
+        self.height_graph.stop = False
+        self.height_graph.plotter()
+       
+    def stop_graphs(self):   
+        self.height_graph.stop = True    
+    
     def invoke_command(self):
-        self.Frame_com_menu = Frame(self.Frame_input,background="white")
+        self.Frame_com_menu = Frame(self.Frame_input,background="gray55")
         self.Frame_com_menu.grid(row = 1, column = 0, rowspan = 3, columnspan = 1, sticky='W')    
         
-        btn_turn = Button(self.Frame_com_menu, text="TURN", command=self.invoke_turn) 
+        btn_turn = Button(self.Frame_com_menu, text="TURN", command=self.invoke_turn,background ="gray11",foreground = "white") 
         btn_turn.config( height = 1, width = 7 ) 
         btn_turn.grid(row = 0, column = 0, padx = 5, pady = 3, columnspan = 1, sticky='W')  
         
-        btn_move = Button(self.Frame_com_menu, text="MOVE", command=self.invoke_move) 
+        btn_move = Button(self.Frame_com_menu, text="MOVE", command=self.invoke_move,background ="gray11",foreground = "white") 
         btn_move.config( height = 1, width = 7 ) 
         btn_move.grid(row = 1, column = 0, padx = 5, pady = 3, columnspan = 1, sticky='W')     
         
-        btn_lift = Button(self.Frame_com_menu, text="LIFT", command=self.invoke_lift) 
+        btn_lift = Button(self.Frame_com_menu, text="LIFT", command=self.invoke_lift,background ="gray11",foreground = "white") 
         btn_lift.config( height = 1, width = 7 ) 
         btn_lift.grid(row = 2, column = 0, padx = 5, pady = 3, columnspan = 1, sticky='W')
         
         self.Frame_cmenu.grid_remove()
 
     def invoke_turn(self):
-        self.Frame_turn_menu = Frame(self.Frame_input,background="white")
+        self.Frame_turn_menu = Frame(self.Frame_input,background="gray55")
         self.Frame_turn_menu.grid(row = 1, column = 0, rowspan = 3, columnspan = 1, sticky='W')    
         
         entry_turn_input = Entry(self.Frame_turn_menu) 
         entry_turn_input.grid(row = 0, column = 0, padx = 3, pady = 3) 
         
-        btn_input_turn_enter = Button(self.Frame_turn_menu, text="ENTER",command=self.invoke_turn_enter) 
+        btn_input_turn_enter = Button(self.Frame_turn_menu, text="ENTER",command=self.invoke_turn_enter,background ="gray11",foreground = "white") 
         btn_input_turn_enter.grid(row = 1, column = 0, padx = 2, pady = 3, columnspan = 3,sticky='W') 
         
         self.Frame_com_menu.grid_remove()
@@ -221,13 +378,13 @@ class GUI(Frame):
         self.Frame_cmenu.grid(row = 1, column = 0, rowspan = 3, columnspan = 1, sticky='W')
         
     def invoke_move(self):
-        self.Frame_move_menu = Frame(self.Frame_input,background="white")
+        self.Frame_move_menu = Frame(self.Frame_input,background="gray55")
         self.Frame_move_menu.grid(row = 1, column = 0, rowspan = 3, columnspan = 1, sticky='W')    
         
         entry_move_input = Entry(self.Frame_move_menu) 
         entry_move_input.grid(row = 0, column = 0, padx = 3, pady = 3) 
         
-        btn_input_move_enter = Button(self.Frame_move_menu, text="ENTER",command=self.invoke_move_enter) 
+        btn_input_move_enter = Button(self.Frame_move_menu, text="ENTER",command=self.invoke_move_enter,background ="gray11",foreground = "white") 
         btn_input_move_enter.grid(row = 1, column = 0, padx = 2, pady = 3, columnspan = 3,sticky='W') 
         
         self.Frame_com_menu.grid_remove()
@@ -238,13 +395,13 @@ class GUI(Frame):
         self.Frame_cmenu.grid(row = 1, column = 0, rowspan = 3, columnspan = 1, sticky='W')
         
     def invoke_lift(self):
-        self.Frame_lift_menu = Frame(self.Frame_input,background="white")
+        self.Frame_lift_menu = Frame(self.Frame_input,background="gray55")
         self.Frame_lift_menu.grid(row = 1, column = 0, rowspan = 3, columnspan = 1, sticky='W')    
         
         entry_lift_input = Entry(self.Frame_lift_menu) 
         entry_lift_input.grid(row = 0, column = 0, padx = 3, pady = 3) 
         
-        btn_input_move_enter = Button(self.Frame_lift_menu, text="ENTER",command=self.invoke_lift_enter) 
+        btn_input_move_enter = Button(self.Frame_lift_menu, text="ENTER",command=self.invoke_lift_enter,background ="gray11",foreground = "white") 
         btn_input_move_enter.grid(row = 1, column = 0, padx = 2, pady = 3, columnspan = 3,sticky='W') 
         
         self.Frame_com_menu.grid_remove()
