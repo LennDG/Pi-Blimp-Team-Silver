8cfc7529299e4cb72bc2f3ad2760349ffbd5cc2b
Lenn <lenn.de.graef@gmail.com>
2013-11-28 15:45:53 +0100

    I think the connection should work now

--- SilverSurfer/GUIConnection.py
+++ SilverSurfer/GUIConnection.py
@@ -1,61 +1,28 @@
-#File for the GUI connection class, this will be housed on the PC
+#This is the client conneciton side, housed on PC
 
-import socket, threading, Queue
+import socket, threading
 
-class GUIConn(object):
+class Client(threading.Thread, object):
     
-    def __init__(self, input_queue, output_queue):
-        
-        self.PORT = 8888
-        self.PI_IP = "192.168.1.1"
-        
-        
-        try:
-            #Creates a socket
-            self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        except socket.error, msg:
-            print 'Socket creating failed, error message:' + str(msg[1])
-        
-        self.s.connect((self.PI_IP, self.PORT))
-        #TODO: Print a socket has been made.
-        
-        inbox = Input(input_queue, self.s)
-        inbox.start()
-        
-        outbox = Output(output_queue, self.s)
-        outbox.start()
-        
-    def close(self):
-        self.s.close()
-        
-        
-class Input(threading.Thread, object):
-    
-    def __init__(self, input_queue, socket):
+    def __init__(self, inqueue, outqueue):
         threading.Thread.__init__(self)
-        self.s = socket
-        self.queue = input_queue
         
-    def run(self):
-        while True:
-            data = self.s.recv(4096)
-            self.queue.put(data)
-            
-class Output(threading.Thread, object):
-    
-    def __init__(self, output_queue, socket):
-        threading.Thread.__init__(self)
-        self.s = socket
-        self.queue = output_queue
+        HOST = '192.168.1.1'
+        PORT = 8888
+        
+        self.inqueue= inqueue
+        self.outqueue = outqueue
+        
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect((HOST, PORT))
         
     def run(self):
         while True:
             try:
-                output = self.queue.get(False)              
-                self.s.sendall(output)
-            except Queue.Empty:
-                pass
-            except socket.error:
-                print "Sending failed"
-            
-            
+                data = self.outqueue.get()
+                self.s.sendall(data)
+                reply = self.s.recv(1024) #BLOCKING CALL
+                self.inqueue.put(reply)
+            except Exception:
+                self.s.close()
+                
--- SilverSurfer/PiConnection.py
+++ SilverSurfer/PiConnection.py
@@ -1,58 +1,33 @@
-#File for the Pi connection class, this will be housed on the Pi
+#This is the server file, it is housed on the Pi
 
-import socket, threading, Queue
+import socket, threading
 
-class PiConn(object):
+class Server(threading.Thread, object):
     
-    def __init__(self, input_queue, output_queue):
-        self.HOST = ''
-        self.PORT = 8888
+    def __init__(self, inqueue, outqueue):
+        threading.Thread.__init__(self)
+        
+        HOST = ''
+        PORT = 8888
+        
+        self.inqueue = inqueue
+        self.outqueue = outqueue
+        
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        
-        try:
-            self.s.bind((self.HOST, self.PORT))
-        except socket.error, msg:
-            print "Binding socket failed, error message: " + msg[1]
-            
-        
-        
-        inbox = Input(input_queue, self.s)
-        inbox.start()
-    
-    def start(self):
-        self.s.listen(1)
-        
-    def stop(self):
-        self.s.close()
-    
-class Input(threading.Thread, object):
-    
-    def __init__(self, input_queue, socket):
-            threading.Thread.__init__(self)
-            self.queue = input_queue
-            self.s = socket
-            
+        self.s.bind((HOST, PORT))
 
     def run(self):
-        while True:
-            conn = self.s.accept()
-            data = conn.recv(1024)
-            self.queue.put(data)
-        
-class Output(threading.Thread, object):
-        
-        def __init__(self, output_queue, socket):
-            threading.Thread.__init__(self)
-            self.queue = output_queue
-            self.s = socket
-            
-        def run(self):
+        self.s.listen(1)
+        conn, addr = self.s.accept() #BLOCKING CALL
+        print 'Connected by', addr
+        try:
             while True:
-                try:
-                    output = self.queue.get(False)              
-                    self.s.sendall(output)
-                except Queue.Empty:
-                    pass
-                except socket.error:
-                    print "Sending failed"
+                data = conn.recv(1024) #BLOCKING CALL
+                if not data: break
+                self.inqueue.put(data)
+                reply = self.outqueue.get(True) #BLOCKING CALL
+                conn.sendall(reply)
+            conn.close()
+        except Exception:
+            conn.close()
             
