7b4cff1a779d5b933d9d9b0f70266bfdf2b8d972
Cookiedude <robcoekaerts@gmail.com>
2014-02-16 16:41:27 +0100

    Verbeterde versie Figure

--- SilverSurfer/Figure.py
+++ SilverSurfer/Figure.py
@@ -1,45 +1,74 @@
 '''
-This class represents the figures that are placed at the nodes in a playfield. The objects of this class
-are characterised by a shape and a color.
+This module contains the class Figure, together with the implementation of colors and strings.
 
 @author     Rob Coekaerts 
 @Version    16-2-2014
+
+'''
+
+
+colors = "blue", "white", "red", "yellow", "black", "green"
+shapes = "heart", "oval", "rectangle", "star"
+    
+'''
+checks whether the given color is a valid color.
+@return: color in colors
+'''
+def is_valid_color(color):
+    return color in colors
+
+'''
+Checks whether the given shape is a valid shape
+@return: shape in shapes
+'''
+def is_valid_shape(shape):
+    return shape in shapes
+
+
+
+'''
+This class represents the figures that are placed at the nodes in a playfield. The objects of this class
+are characterised by a shape and a color.
 
 '''
 class Figure(object):
     
     '''
     Initialise this figure with a specific shape and color using numbers. 
-    Colors are represented in the following way: 
-       blue = 1, white = 2, red = 3, yellow = 4, black = 5, green = 6.
-    Shapes are represented in the following way:
-       heart = 1, oval = 2, rectangle = 3, star = 4.
+    @precondition: The given color must be a valid color
+                   |is_valid_color(color)
+    @precondition: The given shape must be a valid shape
+                   |is_valid_shape(shape)
        
     '''
-    def __init__(self, shape, color):
-        self._shape = shape
-        self._color = color
+    def __init__(self, color, shape):
+        assert is_valid_color(color)
+        assert is_valid_shape(shape)
+        self.shape = shape
+        self.color = color
     
-    '''
-    Return the shape of this object using a number.
-      1 = heart, 2 = oval, 3 = rectangle, 4 = star 
-    '''  
-    @property    
-    def shape(self):
-        return self._shape
-    
-    '''
-    Return the color of this object using a number.
-      1 = blue, 2 = white, 3 = red, 4 = yellow, 5 = black, 6 = green. 
-    '''  
-    @property
-    def color(self):
-        return self._color
-    
-    '''
+
+    '''    
     Return a number that identifies the specific figure uniquely.
-    @return: self.shape*10 + self.color
     '''
-    def identity(self):
-        return self.shape*10 + self.color
+    @property
+    def figure_id(self):
+        decimal = shapes.index(self.shape)
+        unit = colors.index(self.color)
+        return decimal*10 + unit
+    
+    
+    # Overrides the built-in equals method
+    def __eq__(self, other):
+        if isinstance(other, Figure):
+            return self.figure_id == other.figure_id
+        return NotImplemented
+
+    # Overrides the built-in not-equals method
+    def __ne__(self, other):
+        result = self.__eq__(other)
+        if result is NotImplemented:
+            return result
+        return not result
+    
     
