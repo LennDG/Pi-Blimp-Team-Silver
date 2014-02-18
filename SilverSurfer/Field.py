26fb1cdf2a8e67d7e1398ddcb7051332c559ce4e
Cookiedude <robcoekaerts@gmail.com>
2014-02-18 14:36:29 +0100

    Figure_id enzo

--- SilverSurfer/Field.py
+++ SilverSurfer/Field.py
@@ -55,6 +55,50 @@ class Node(object):
                 right_neighbour = (relative_position + 1)%6 #Calculate the index of the right neighbour of the new node
                 self.neighbours[left_neighbour].add(new_node, relative_position + 2)  #add the node to the correct position on the left neighbour.
                 self.neighbours[right_neighbour].add(new_node, relative_position - 2)  #add the node to the correct position on the left neighbour.
+      
+      
+    def iterate_row(self, value, direction=1):
+        relative_position = int(0.5 + 1.5*direction)%6
+        current_node = self
+        results = []
+        while current_node.neighbours[relative_position] != 0:
+            current_node = current_node.neighbours[relative_position]
+            if current_node.value == value:
+                results.append(current_node)
+        return results, current_node
+        
+    def row_extreme(self, direction):
+        relative_position = int(0.5 + 1.5*direction)%6
+        current_node = self
+        while current_node.neighbours[relative_position] != 0:
+            current_node = current_node.neighbours[relative_position]
+        return current_node
+    
+    def next_row(self, vert_direction):  # 1 is up, -1 is down
+        relative_position = int(4.5 + vert_direction*1.5)%6
+        if self.neighbours[relative_position] == 0:
+            relative_position += 1
+        return self.neighbours[relative_position]  # If there is still no neighbour, it will
+    # return 0 and there is no next row.
+        
+    def iterate_field(self, value, vert_direction, hor_direction):
+        current_node = self
+        intermediate_results = []
+        results = []
+        while current_node.next_row(vert_direction) != 0 or current_node == self:
+            intermediate_results, current_node = current_node.iterate_left_right(value, hor_direction)
+            for result in intermediate_results:
+                results.append(result)
+            current_node = current_node.next_row(-1).row_extreme(hor_direction)
+            hor_direction = hor_direction*-1
+        return results
+    
+    def iterate_concentrically(self, relative_direction, value):
+        current_node = self
+        
+            
+            
+            
                 
                 
 
