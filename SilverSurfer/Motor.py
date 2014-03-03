644d473f08cafe1da95f1a2a1781a9938b191242
Lenn <lenn.de.graef@gmail.com>
2014-03-03 16:14:35 +0100

    sdsd

--- SilverSurfer/Motor.py
+++ SilverSurfer/Motor.py
@@ -10,7 +10,7 @@ class Motor(threading.thread, object):
         
         GPIO.setmode(GPIO.BCM)
         
-        self.level = 0
+        self.level = 0.0
         
         self.frequency = frequency
         
