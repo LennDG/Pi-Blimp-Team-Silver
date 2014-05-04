1dfb46519b7e3aec1d57ff1a4d745c0c3ccfe1d4
GiuseppeCallari <mcjosi@hotmail.com>
2014-05-05 00:59:57 +0200

    nog iets in PID

--- SilverSurfer/PID.py
+++ SilverSurfer/PID.py
@@ -68,7 +68,7 @@ class PID(object):
         # Calculate the differential term, being careful not to divide by 0
         self.Cd = 0
         if dt > 0:
-            self.Cd = de/dt
+            self.Cd = de/dtself.motor_output(error)
             
         # Save the time and error for the next time the function runs
         self.prev_time = self.current_time
